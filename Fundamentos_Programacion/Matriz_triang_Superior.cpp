8b9e547a9f03a61b064c748ea7eb80928a7ec9d5
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-07-31 21:48:45 -0500

    Ejercicio de matriz para triangulo superior

---
+++ Fundamentos_Programacion/Matriz_triang_Superior.cpp
@@ -0,0 +1,29 @@
+/*10. Escribe un programa para encontrar la matriz triangular superior.*/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	int i, j;
+	float mat[4][4];
+	cout<<"Ingrese la matriz (3*3)"<<endl;
+	for (i=1;i<=3;i++) {
+		for (j=1;j<=3;j++) {
+			cin >> mat[i][j];
+		}
+	}
+	cout << "Los elementos de la matriz son:" << endl;
+	for (i=1;i<=3;i++) {
+		for (j=1;j<=3;j++) {
+			cout << mat[i][j] << " ";
+		}
+		cout << "" << endl;
+	}
+	cout << "Triangular Superior" << endl;
+	for (i=1;i<=3-1;i++) {
+		for (j=i+1;j<=3;j++) {
+			cout << mat[i][j] << " ";
+		}
+	}
+	return 0;
+}
