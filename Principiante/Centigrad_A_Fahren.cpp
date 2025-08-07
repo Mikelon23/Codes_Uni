5f8ff2dd9a641a2ead830e6dbc19274bba196be7
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-08-06 19:48:56 -0500

    Ejercicios de Cent a Fahren

---
+++ Principiante/Centigrad_A_Fahren.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double C, F;
+    cin >> C;
+    F = (C * 1.8) + 32;
+    cout << C << " grados centigrados equivalen a " << F << " grados Fahrenheit" << endl;
+    return 0;
+}
