62b21ea594cb194dcd50f2a08e7731d5512b5037
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-08-06 19:47:40 -0500

    Ejercicios de kb a mb

---
+++ Principiante/Kb_A_Mb.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double Kb, Mb;
+	cin >> Kb;
+	Mb = Kb / 1024;
+	cout << Kb << " Kbytes equivalen a " << Mb << " Megabytes" << endl;
+	return 0;
+}
