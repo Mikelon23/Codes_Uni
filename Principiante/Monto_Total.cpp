da34c33249c2c6f6ed96a012f4e4733f1aa11bb7
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-08-06 19:52:19 -0500

    Ejercicios para determinar el monto toal

---
+++ Principiante/Monto_Total.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double cantidad, cantfinal;
+	cin >> cantidad;
+	cantfinal = cantidad + (cantidad * 0.2);
+	cout << "El monto final es: " << cantfinal << endl;
+	return 0;
+}
