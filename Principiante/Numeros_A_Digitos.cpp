cf93adcfb0bde5278eac7763e5fd60784caafa15
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-08-10 22:49:39 -0500

    numeros a digitos

---
+++ Principiante/Numeros_A_Digitos.cpp
@@ -0,0 +1,39 @@
+// Separa un numero entero en sus digitos
+
+#include <iostream>
+#include <cmath> // para pow y trunc
+using namespace std;
+
+int main() {
+    int i, digito, pot, n, aux, cont;
+
+    cout << "Ingrese un numero entero positivo: ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "El numero debe ser positivo." << endl;
+        return 0;
+    }
+
+    // Contar cuántos dígitos tiene
+    cont = 0;
+    aux = n;
+    while (aux > 0) {
+        cont++;
+        aux = aux / 10; // división entera
+    }
+
+    cout << "El numero tiene " << cont << " digitos" << endl;
+
+    // Mostrar cada dígito uno por uno
+    aux = n;
+    for (i = 1; i <= cont; i++) {
+        pot = static_cast<int>(pow(10, cont - i)); // potencia de 10
+        digito = aux / pot; // obtener el dígito
+        cout << digito << " ";
+        aux = aux - digito * pot; // quitar ese dígito
+    }
+    cout << endl;
+
+    return 0;
+}
