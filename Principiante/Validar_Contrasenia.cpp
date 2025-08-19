c56e9d74e121604e6a41e2f5a25da25261236df0
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-08-18 22:40:32 -0500

    ejercicio para vaidar una contraseña con 3 intentos

---
+++ Principiante/Validar_Contrasenia.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string clave, cont = "abc123";
+    int c = 0;
+
+    do {
+        if (c != 3) {
+            cout << "Ingrese la contraseña: ";
+            cin >> clave;
+
+            if (clave != cont) {
+                cout << "Error, vuelva a ingresar la clave" << endl;
+                c++;
+            }
+        } else {
+            cout << "Se bloqueo su clave, ha llegado al maximo numero de intentos" << endl;
+        }
+    } while (clave != cont && c < 3);
+
+    if (c != 3) {
+        cout << "Bienvenido al sistema" << endl;
+    } else {
+        cout << "Finalizo el sistema, Intentelo mas tarde" << endl;
+    }
+
+    return 0;
+}
