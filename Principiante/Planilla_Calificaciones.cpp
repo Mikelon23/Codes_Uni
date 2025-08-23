a9c965e7423b483a1459802eb3484f43fb6612ac
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-08-22 22:25:37 -0500

    ejercicio de planilla de calificaciones

--- Principiante/Planilla_Calificaciones.cpp
+++ Principiante/Planilla_Calificaciones.cpp
@@ -98,3 +98,120 @@ int buscar_codigo(int cod, const vector<int>& vc) {
     }
     return -1;
 }
+// ================== PROGRAMA PRINCIPAL ==================
+int main() {
+    int n;
+    cout << "Ingrese el numero de estudiantes: ";
+    cin >> n;
+
+    vector<vector<double>> mat(n, vector<double>(4, 0));
+    vector<string> vec(n);
+    vector<int> vcod(n);
+    vector<double> vecnotas(n);
+    vector<int> vecpos(n);
+
+    int op, op2, cod, pos;
+    string PalabraClave = "";
+
+    do {
+        menu();
+        cin >> op;
+
+        switch (op) {
+            case 1:
+                cout << "Ingrese las notas de cada estudiante\n";
+                generar_codigos(vcod);
+                ingreso_nombres_notas(vec, mat);
+                break;
+
+            case 2:
+                cout << "Los codigos son:\n";
+                visualizar_vector(vcod);
+                cout << "Los estudiantes son:\n";
+                visualizar_vector(vec);
+                break;
+
+            case 3:
+                cout << "Las notas son:\n";
+                visualizar_notas(mat);
+                break;
+
+            case 4:
+                cout << "Ingrese el codigo a buscar: ";
+                cin >> cod;
+                pos = buscar_codigo(cod, vcod);
+                if (pos != -1)
+                    visualizar_notasxcodigo(vec, mat, pos);
+                else
+                    cout << "Error. El codigo no existe.\n";
+                break;
+
+            case 5:
+                cout << "Los estudiantes ordenados alfabeticamente son:\n";
+                ordenar_vector(vec);
+                visualizar_vector(vec);
+                break;
+
+            case 6:
+                for (int i = 0; i < n; i++) {
+                    vecpos[i] = i;
+                    vecnotas[i] = mat[i][3];
+                }
+                ordenar_vectorxnotas(vecnotas, vecpos);
+                cout << "Los estudiantes ordenados por notas descendente:\n";
+                cout << "Notas:\n";
+                for (auto nota : vecnotas) cout << nota << "\n";
+                cout << "Nombres:\n";
+                visualizar_nombres(vecpos, vec);
+                break;
+
+            case 7:
+                cout << "Ingrese el codigo a buscar: ";
+                cin >> cod;
+                pos = buscar_codigo(cod, vcod);
+                if (pos != -1) {
+                    visualizar_notasxcodigo(vec, mat, pos);
+                    cout << "Que nota desea modificar?\n";
+                    cout << "1. Nota 1\n2. Nota 2\n3. Nota 3\n";
+                    cout << "Seleccione su opcion: ";
+                    cin >> op2;
+                    double nuevaNota;
+                    switch (op2) {
+                        case 1:
+                            cout << "Ingrese el nuevo valor de la nota 1: ";
+                            cin >> nuevaNota;
+                            mat[pos][0] = nuevaNota;
+                            break;
+                        case 2:
+                            cout << "Ingrese el nuevo valor de la nota 2: ";
+                            cin >> nuevaNota;
+                            mat[pos][1] = nuevaNota;
+                            break;
+                        case 3:
+                            cout << "Ingrese el nuevo valor de la nota 3: ";
+                            cin >> nuevaNota;
+                            mat[pos][2] = nuevaNota;
+                            break;
+                        default:
+                            cout << "Error, ingrese un valor entre 1 y 3\n";
+                    }
+                    mat[pos][3] = (mat[pos][0] + mat[pos][1] + mat[pos][2]) / 3;
+                    cout << "Los nuevos datos modificados son:\n";
+                    visualizar_notasxcodigo(vec, mat, pos);
+                } else {
+                    cout << "Error. El codigo no existe.\n";
+                }
+                break;
+
+            case 8:
+                PalabraClave = "Salir";
+                break;
+
+            default:
+                cout << "Error, ingrese un valor entre 1 y 8\n";
+        }
+
+    } while (PalabraClave != "Salir");
+
+    return 0;
+}
