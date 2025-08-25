457eb8c6ff418f240eee552c8f4dbceb86b44ef5
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-08-24 21:57:41 -0500

    ejercicio de bucle while para contar aprobados

---
+++ Principiante/While_Aprobados.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Título del programa
+    cout << "===== CONTAR ESTUDIANTES APROBADOS =====" << endl;
+
+    int n, i = 1, ca = 0;  // n = cantidad de estudiantes, ca = contador de aprobados
+    double nota;
+
+    cout << "Ingrese el numero de estudiantes: ";
+    cin >> n;
+
+    while (i <= n) {
+        cout << "Ingrese la nota del estudiante " << i << ": ";
+        cin >> nota;
+
+        if (nota >= 7.0) {
+            ca++;
+        }
+
+        i++;
+    }
+
+    cout << "\nDe los " << n << " estudiantes, pasaron unicamente " << ca << "." << endl;
+
+    return 0;
+}
