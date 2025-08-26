6c8dd52072bdb1fd5bf0e71bcfddacc887d16311
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-08-25 22:42:36 -0500

    ejercicio para dibujar un triaungulo

---
+++ Principiante/Dibujar_Triaungulo.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Título del programa
+    cout << "===== DIBUJA UN TRIANGULO =====" << endl;
+
+    int b;
+    cout << "Ingrese la base del triangulo: ";
+    cin >> b;
+
+    // Dibujar triángulo
+    for (int i = 1; i <= b; i++) {
+        // Espacios en blanco
+        for (int j = 1; j <= b - i; j++) {
+            cout << " ";
+        }
+        // Asteriscos con espacio
+        for (int k = 1; k <= i; k++) {
+            cout << "* ";
+        }
+        cout << endl; // salto de línea
+    }
+
+    return 0;
+}
