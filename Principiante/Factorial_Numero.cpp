b0289e396720f90925d7a99ad3e957bf6ef23d49
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-08-27 21:27:06 -0500

    ejercicio de factorizar un numero

---
+++ Principiante/Factorial_Numero.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Título del programa
+    cout << "===== FACTORIAL DE UN NUMERO =====" << endl;
+
+    int n, fact = 1;
+
+    cout << "Ingrese el numero para determinar su factorial: ";
+    cin >> n;
+
+    for (int i = n; i >= 1; i--) {
+        fact *= i;
+    }
+
+    cout << "\nEl factorial del numero " << n << " es: " << fact << endl;
+
+    return 0;
+}
