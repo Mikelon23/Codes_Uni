e8e394cd60b38236b49bf7919955efa0d1906b98
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-08-28 23:42:49 -0500

    ejercicio de manejo de vectores

--- Principiante/Manejo_Vectores.cpp
+++ Principiante/Manejo_Vectores.cpp
@@ -10,3 +10,19 @@ void ingresar_vector(vector<int> &v) {
         cin >> v[i];
     }
 }
+// Función para ingresar elementos de un vector real
+void ingresar_vector(vector<double> &v) {
+    for (size_t i = 0; i < v.size(); i++) {
+        cout << "Ingrese el valor " << i + 1 << ": ";
+        cin >> v[i];
+    }
+}
+
+// Función para ingresar elementos de un vector de cadenas
+void ingresar_vector(vector<string> &v) {
+    cin.ignore(); // limpiar buffer
+    for (size_t i = 0; i < v.size(); i++) {
+        cout << "Ingrese el valor " << i + 1 << ": ";
+        getline(cin, v[i]);
+    }
+}
