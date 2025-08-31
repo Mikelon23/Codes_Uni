3fd5135f24b9670cc7c6cd9750145ba5750dc650
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-08-30 22:54:17 -0500

    programa de calificaciones para obtener promedios

--- Principiante/Calificaciones_Promedio.cpp
+++ Principiante/Calificaciones_Promedio.cpp
@@ -13,3 +13,18 @@ int main() {
 
     double st = 0;
 
+    // Ingreso de nombres y notas
+    for (int i = 0; i < m; i++) {
+        double s = 0;
+        cout << "\nIngrese el nombre del estudiante " << i + 1 << ": ";
+        cin.ignore(); // limpiar buffer
+        getline(cin, vnombres[i]);
+
+        for (int j = 0; j < 3; j++) {
+            cout << "Ingrese la nota " << j + 1 << ": ";
+            cin >> mat[i][j];
+            s += mat[i][j];
+        }
+        mat[i][3] = s / 3; // promedio del estudiante
+        st += mat[i][3];   // acumular para promedio del grupo
+    }
