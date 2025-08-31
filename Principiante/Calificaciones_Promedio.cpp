bb221f88944f55990983888b93957184ab4a32bf
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-08-30 22:54:05 -0500

    programa de calificaciones para obtener promedios

--- Principiante/Calificaciones_Promedio.cpp
+++ Principiante/Calificaciones_Promedio.cpp
@@ -2,3 +2,14 @@
 #include <vector>
 #include <string>
 using namespace std;
+int main() {
+    int m;
+    cout << "===== NOTAS DE ESTUDIANTES =====" << endl;
+    cout << "Ingrese el numero de estudiantes: ";
+    cin >> m;
+
+    vector<vector<double>> mat(m, vector<double>(4, 0)); // 3 notas + promedio
+    vector<string> vnombres(m);
+
+    double st = 0;
+
