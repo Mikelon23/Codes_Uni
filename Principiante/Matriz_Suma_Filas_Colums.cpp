963aff8d340a02c3ca833f00219304ba2b20c2fd
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-08-30 22:58:04 -0500

    ejercicio de matrices -suma de filas y columnas

--- Principiante/Matriz_Suma_Filas_Colums.cpp
+++ Principiante/Matriz_Suma_Filas_Colums.cpp
@@ -23,3 +23,42 @@ int main() {
             cin >> mat[i][j];
         }
     }
+
+    // Mostrar matriz
+    cout << "\nLos elementos de la matriz original son:" << endl;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << mat[i][j] << "   ";
+        }
+        cout << endl;
+    }
+
+    // Sumar filas
+    for (int i = 0; i < m; i++) {
+        int s = 0;
+        for (int j = 0; j < n; j++) {
+            s += mat[i][j];
+        }
+        vsf[i] = s;
+    }
+
+    cout << "\nLos elementos del vector suma filas son:" << endl;
+    for (int i = 0; i < m; i++)
+        cout << vsf[i] << endl;
+
+    // Sumar columnas
+    for (int j = 0; j < n; j++) {
+        int s = 0;
+        for (int i = 0; i < m; i++) {
+            s += mat[i][j];
+        }
+        vsc[j] = s;
+    }
+
+    cout << "\nLos elementos del vector suma columnas son:" << endl;
+    for (int j = 0; j < n; j++)
+        cout << vsc[j] << " ";
+    cout << endl;
+
+    return 0;
+}
