77d15df054149fa5ea09b3f72c1ce38013117523
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-09-02 18:18:40 -0500

    ejercicio para poder modificar calificaciones

--- Principiante/Modificar_Notas.cpp
+++ Principiante/Modificar_Notas.cpp
@@ -13,3 +13,20 @@ int main() {
     int n;
     cout << "Ingrese el numero de estudiantes: ";
     cin >> n;
+
+    vector<Estudiante> estudiantes(n);
+
+    // Inicializar códigos y nombres
+    for(int i = 0; i < n; i++) {
+        estudiantes[i].codigo = 100 + i + 1;
+        cout << "Ingrese el nombre del estudiante " << i+1 << ": ";
+        cin >> estudiantes[i].nombre;
+        estudiantes[i].notas.resize(4); // 3 parciales + promedio
+        double suma = 0;
+        for(int j = 0; j < 3; j++) {
+            cout << "Ingrese nota " << j+1 << ": ";
+            cin >> estudiantes[i].notas[j];
+            suma += estudiantes[i].notas[j];
+        }
+        estudiantes[i].notas[3] = suma / 3.0; // promedio
+    }
