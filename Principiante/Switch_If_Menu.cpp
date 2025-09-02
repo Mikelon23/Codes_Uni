dc070af4bcba0b0b164e30505386e583ee6dfb04
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-09-01 23:50:44 -0500

    ejercicio de switch option

---
+++ Principiante/Switch_If_Menu.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int op;
+    double sueldo, ns;
+
+    cout << "===== MENÚ PRINCIPAL =====" << endl;
+    cout << "1. VENDEDORES" << endl;
+    cout << "2. DISEÑADORES" << endl;
+    cout << "3. ADMINISTRATIVOS" << endl;
+    cout << "Escoja su opción: ";
+    cin >> op;
