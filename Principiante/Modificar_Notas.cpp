442c55f2e6ed7748b6a69c6de5e896e13fe385ef
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-09-02 18:20:03 -0500

    ejercicio para poder modificar calificaciones

--- Principiante/Modificar_Notas.cpp
+++ Principiante/Modificar_Notas.cpp
@@ -3,13 +3,15 @@
 #include <string>
 using namespace std;
 
-struct Estudiante {
+struct Estudiante
+{
     int codigo;
     string nombre;
     vector<double> notas; // notas[0..2] y notas[3] promedio
 };
 
-int main() {
+int main()
+{
     int n;
     cout << "Ingrese el numero de estudiantes: ";
     cin >> n;
@@ -17,14 +19,16 @@ int main() {
     vector<Estudiante> estudiantes(n);
 
     // Inicializar códigos y nombres
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         estudiantes[i].codigo = 100 + i + 1;
-        cout << "Ingrese el nombre del estudiante " << i+1 << ": ";
+        cout << "Ingrese el nombre del estudiante " << i + 1 << ": ";
         cin >> estudiantes[i].nombre;
         estudiantes[i].notas.resize(4); // 3 parciales + promedio
         double suma = 0;
-        for(int j = 0; j < 3; j++) {
-            cout << "Ingrese nota " << j+1 << ": ";
+        for (int j = 0; j < 3; j++)
+        {
+            cout << "Ingrese nota " << j + 1 << ": ";
             cin >> estudiantes[i].notas[j];
             suma += estudiantes[i].notas[j];
         }
@@ -33,11 +37,12 @@ int main() {
 
     // Mostrar todos los estudiantes
     cout << "\nDatos de los estudiantes:\n";
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cout << "Codigo: " << estudiantes[i].codigo
              << ", Nombre: " << estudiantes[i].nombre << endl;
-        for(int j = 0; j < 3; j++)
-            cout << "Nota " << j+1 << ": " << estudiantes[i].notas[j] << "  ";
+        for (int j = 0; j < 3; j++)
+            cout << "Nota " << j + 1 << ": " << estudiantes[i].notas[j] << "  ";
         cout << "Promedio: " << estudiantes[i].notas[3] << endl;
     }
 
@@ -46,20 +51,25 @@ int main() {
     cout << "\nIngrese el codigo a buscar: ";
     cin >> cod;
     int pos = -1;
-    for(int i = 0; i < n; i++) {
-        if(estudiantes[i].codigo == cod) {
+    for (int i = 0; i < n; i++)
+    {
+        if (estudiantes[i].codigo == cod)
+        {
             pos = i;
             break;
         }
     }
 
-    if(pos != -1) {
+    if (pos != -1)
+    {
         cout << "\nEstudiante encontrado:\n";
         cout << "Nombre: " << estudiantes[pos].nombre << endl;
-        for(int j = 0; j < 3; j++)
-            cout << "Nota parcial " << j+1 << ": " << estudiantes[pos].notas[j] << endl;
+        for (int j = 0; j < 3; j++)
+            cout << "Nota parcial " << j + 1 << ": " << estudiantes[pos].notas[j] << endl;
         cout << "Definitiva: " << estudiantes[pos].notas[3] << endl;
-    } else {
+    }
+    else
+    {
         cout << "Codigo no encontrado.\n";
         return 0;
     }
@@ -69,22 +79,26 @@ int main() {
     cout << "\nQue nota desea modificar?\n1. Nota 1\n2. Nota 2\n3. Nota 3\nSeleccione su opcion: ";
     cin >> op;
 
-    if(op >= 1 && op <= 3) {
+    if (op >= 1 && op <= 3)
+    {
         double nuevaNota;
         cout << "Ingrese el nuevo valor de la nota " << op << ": ";
         cin >> nuevaNota;
-        estudiantes[pos].notas[op-1] = nuevaNota;
+        estudiantes[pos].notas[op - 1] = nuevaNota;
         // recalcular promedio
         estudiantes[pos].notas[3] = (estudiantes[pos].notas[0] +
                                      estudiantes[pos].notas[1] +
-                                     estudiantes[pos].notas[2]) / 3.0;
+                                     estudiantes[pos].notas[2]) /
+                                    3.0;
 
         cout << "\nDatos modificados:\n";
         cout << "Nombre: " << estudiantes[pos].nombre << endl;
-        for(int j = 0; j < 3; j++)
-            cout << "Nota parcial " << j+1 << ": " << estudiantes[pos].notas[j] << endl;
+        for (int j = 0; j < 3; j++)
+            cout << "Nota parcial " << j + 1 << ": " << estudiantes[pos].notas[j] << endl;
         cout << "Definitiva: " << estudiantes[pos].notas[3] << endl;
-    } else {
+    }
+    else
+    {
         cout << "Error, ingrese un valor entre 1 y 3\n";
     }
 
