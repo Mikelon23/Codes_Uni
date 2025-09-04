a50f03ebdfb894410353fc2734af1da596817129
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-09-03 22:46:47 -0500

    ejercicio de una matriz para ordenar su diagonal secundaria

--- Principiante/Matriz_Orden_Diagonal_Secund.cpp
+++ Principiante/Matriz_Orden_Diagonal_Secund.cpp
@@ -27,3 +27,26 @@ void lleno_vector(const vector<vector<int>> &mat, vector<int> &vaux) {
         for(int j = 0; j < p; j++)
             vaux[c++] = mat[i][j];
 }
+
+void ordena_vector(vector<int> &vaux) {
+    int n = vaux.size();
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n-1; j++)
+            if(vaux[j] < vaux[j+1])
+                swap(vaux[j], vaux[j+1]);
+}
+
+void lleno_matriz(vector<vector<int>> &mat, const vector<int> &vaux) {
+    int p = mat.size();
+    int c = 0;
+    for(int i = 0; i < p; i++)
+        for(int j = 0; j < p; j++)
+            mat[i][j] = vaux[c++];
+}
+
+void diagonal_secundaria(const vector<vector<int>> &mat) {
+    int p = mat.size();
+    for(int i = 0, j = p-1; i < p; i++, j--)
+        cout << mat[i][j] << "  ";
+    cout << endl;
+}
