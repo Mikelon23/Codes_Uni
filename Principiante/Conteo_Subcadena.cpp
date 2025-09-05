7b27d7b39b3dcc8c52071e39649c7bb4bfcefe5e
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-09-04 23:29:44 -0500

    ejercicio para contar posiciones de subcadenas

--- Principiante/Conteo_Subcadena.cpp
+++ Principiante/Conteo_Subcadena.cpp
@@ -16,7 +16,7 @@ int main() {
     // Subcadena desde la posición 5 hasta el final
     if (longCad1 >= 5) {
         cad2 = cad1.substr(4); // substr(4) toma desde el índice 4 hasta el final
-        cout << "La subcadena desde la posición 5 hasta el final es: " << cad2 << endl;
+        cout << "La subcadena desde la posicion 5 hasta el final es: " << cad2 << endl;
     } else {
         cout << "La cadena tiene menos de 5 caracteres, no se puede extraer subcadena." << endl;
     }
