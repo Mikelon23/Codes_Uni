c97c3a5978f358912b8b36e9cd745d8ca8d82092
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-09-04 23:28:56 -0500

    ejercicio para contar posiciones de subcadenas

---
+++ Principiante/Conteo_Subcadena.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string cad1, cad2, cad3;
+
+    // Ingreso de la primera cadena
+    cout << "Ingrese la primera cadena: ";
+    getline(cin, cad1);
+
