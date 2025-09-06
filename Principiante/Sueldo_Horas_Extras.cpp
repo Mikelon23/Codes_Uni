427590caffb88d38087544ad47ab67c4626fa496
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-09-05 22:56:00 -0500

    ejercicio para calcular el sueldo de horas extras

---
+++ Principiante/Sueldo_Horas_Extras.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double s, st, hd, hn, chd, chn, td, tn;
+    int h;
+
+    cout << "Ingrese su sueldo base: ";
+    cin >> s;
+
+    cout << "Cuantas horas extras en la mañana del mes: ";
+    cin >> hd;
+
+    cout << "Cuantas horas extras en la noche del mes: ";
+    cin >> hn;
+
+    cout << "Ingrese el valor de la hora normal: ";
+    cin >> h;
