f26d4f460ffe633ce49d2594dd2d122b6a75b582
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-09-06 23:57:14 -0500

    ejercicio de un vector inverso

---
+++ Principiante/Vector_Inverso.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, s = 0;
+    double prom;
+
+    cout << "Ingrese el numero de elementos del vector: ";
+    cin >> n;
+
+    vector<int> v(n);
