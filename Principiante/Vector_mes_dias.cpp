466721d5a12fd0f5058b059a60aad4cdf7e111ef
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-09-16 23:24:34 -0500

    ejercicio de un vector con meses y dias

---
+++ Principiante/Vector_mes_dias.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string v_mes[12];
+    int v_dias[12];
+    int n_mes;
+
+    // Inicializamos los meses
+    v_mes[0] = "Enero";
+    v_mes[1] = "Febrero";
+    v_mes[2] = "Marzo";
+    v_mes[3] = "Abril";
+    v_mes[4] = "Mayo";
+    v_mes[5] = "Junio";
+    v_mes[6] = "Julio";
+    v_mes[7] = "Agosto";
+    v_mes[8] = "Septiembre";
+    v_mes[9] = "Octubre";
+    v_mes[10] = "Noviembre";
+    v_mes[11] = "Diciembre";
+
