3b2eda651fbee1ef8999fa0e0879e960e3ac3012
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-09-17 23:39:42 -0500

    ejercicio de contar asistencia para aprobar el semestre

--- Principiante/Asistencia_Aprueba.cpp
+++ Principiante/Asistencia_Aprueba.cpp
@@ -7,3 +7,10 @@ int main() {
 
     cout << "Ingrese el porcentaje de asistencia: ";
     cin >> a;
+
+    cout << "Ingrese su nota final: ";
+    cin >> nf;
+
+    if (a >= 80 && nf >= 8 && nf <= 10) {
+        cout << "Aprobo la materia" << endl;
+    } else {
