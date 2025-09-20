a9525d19d59f8ccf7501ebcce2dcd413f3bc3921
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-09-19 23:55:57 -0500

    ejercicio de operaciones de ficheros binarios"

--- Principiante/Ficheros_Binarios.cpp
+++ Principiante/Ficheros_Binarios.cpp
@@ -20,3 +20,16 @@ int main(){
 	ofstream f2;
 	f1.open("entrada.dat", ios::binary);
 	f2.open("salida.dat", ios::binary);
+
+	//a. Escribir el dato entero x en el fichero.
+	x=349;
+	f2.write((char*)&x,sizeof(int));
+	//b. Escribir el dato double f en el fichero.
+	f=3.2332;
+	f2.write((char*)&f,sizeof(double));
+	//c. Escribir los 5 primeros elementos del array a en el fichero.
+	f2.write((char*)&a[0],5*sizeof(int));
+	//d. Escribir los 10 primeros caracteres de la cadena nombre en el fichero.
+	f2.write(nombre,10);
+	//e. Escribir el dato de tipo Tdato p en el fichero.
+	p.b=894;
